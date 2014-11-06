763cb916c1a61b4035ee80e3ac9f508183e2b868
Kai INUI <lied.der.optik@gmail.com>
2014-11-06 23:06:09 +0900

    pod

---
+++ AQSyncableObjectCoordinator/Classes/AQSyncableObjectCoodinator.m
@@ -0,0 +1,13 @@
+//
+//  AQSyncableObjectCoodinator.m
+//  AQSyncableObjectCoordinator
+//
+//  Created by kaiinui on 2014/11/06.
+//  Copyright (c) 2014年 Aquamarine. All rights reserved.
+//
+
+#import "AQSyncableObjectCoodinator.h"
+
+@implementation AQSyncableObjectCoodinator
+
+@end
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '7.0'
+
+target "AQSyncableObjectCoordinator" do
+  pod "Aquasync"
+end
+
+target "AQSyncableObjectCoordinatorTests" do
+  pod "OCMock"
+  pod "Expecta"
+end
